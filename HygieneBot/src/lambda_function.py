419c6cf46e064dbafc35db9fdea05db1bb8f059b
JakeLDev <me@jakelyell.dev>
2022-06-15 00:48:17 +0800

    Forgot how Python Dicts work
    Fixed it :)

--- HygieneBot/src/lambda_function.py
+++ HygieneBot/src/lambda_function.py
@@ -10,18 +10,10 @@ ROOT = Path(__file__).resolve().parents[0]
 
 def get_tweet(tweets_file, excluded_tweets=None):
     """Get tweet to post from CSV file"""
+    with open(tweets_file) as f:
+        tweet = f.read()
 
-    with open(tweets_file) as csvfile:
-        reader = csv.DictReader(csvfile)
-        possible_tweets = [row["tweet"] for row in reader]
-
-    if excluded_tweets:
-        recent_tweets = [status_object.text for status_object in excluded_tweets]
-        possible_tweets = [tweet for tweet in possible_tweets if tweet not in recent_tweets]
-
-    selected_tweet = random.choice(possible_tweets)
-
-    return selected_tweet
+    return tweet
 
 
 def lambda_handler(event, context):
@@ -32,22 +24,18 @@ def lambda_handler(event, context):
     access_token_secret = os.getenv("ACCESS_TOKEN_SECRET")
 
     print("Authenticate")
-    # auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    # auth.set_access_token(access_token, access_token_secret)
-    # api = tweepy.API(auth)
-
     client = tweepy.Client(consumer_key = consumer_key,
                     consumer_secret = consumer_secret,
                     access_token = access_token,
                     access_token_secret = access_token_secret)
 
     print("Get tweet from csv file")
-    tweets_file = ROOT / "tweets.csv"
-    # recent_tweets = api.user_timeline()[:3]
+    tweets_file = ROOT / event["HygieneType"] # Make message the name of the file, e.g. ToothbrushSwap.txt
+    # tweets_file = "src/ToothbrushSwap.txt" # For Local Testing
     tweet = get_tweet(tweets_file)
 
     print(f"Post tweet: {tweet}")
-    # api.update_status(tweet)
     response = client.create_tweet(text=tweet)
+
 
     return {"statusCode": 200, "tweet": tweet}
